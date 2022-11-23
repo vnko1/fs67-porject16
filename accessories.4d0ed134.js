9ebe45ce99eb4707beb9b21bf5355020f5ead6f4
vnko1 <102794464+vnko1@users.noreply.github.com>
2022-11-23 08:38:01 +0000

    Deploying to gh-pages from @ vnko1/fs67-project16@24011117f349c9d54eafa1750e5871b7c6e3da48 🚀

---
+++ accessories.4d0ed134.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=accessories.4d0ed134.js.map
---
+++ accessories.616e29ea.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=accessories.616e29ea.js.map
