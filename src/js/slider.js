5b5b7a2bd9deec1961271896b9421e607f227abe
Inna Makarenko <macksi@ukr.net>
2022-11-23 09:20:33 +0200

    adds slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,1 +1,14 @@
-$('.section-banners__sliders').section-banners__sliders();
+// var swiper = new Swiper(".mySwiper", {
+//         pagination: {
+//           el: ".swiper-pagination",
+//         dynamicBullets: true,
+//           clickable: true,
+//         },
+//       });
+      var swiper = new Swiper(".mySwiper", {
+        pagination: {
+              el: ".swiper-pagination",
+                      clickable: true,
+
+        },
+      });
